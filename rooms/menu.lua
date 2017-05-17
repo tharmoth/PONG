40d4319875d164f44b30373f2e52444e9d24450b
tharmoth <tharmoth@gmail.com>
2017-05-17 13:50:19 -0500

    Update menu.lua

--- rooms/menu.lua
+++ rooms/menu.lua
@@ -5,21 +5,18 @@ function Menu:new()
     self.area = Area()
     
     --!Build Board
-    self.area:addGameObject('Score', 400-150, 80, {scoreCount = 'P', name = 'P'})
-    self.area:addGameObject('Score', 400-70,  80, {                  name = 'O'})
-    self.area:addGameObject('Score', 400+10,  80, {scoreCount = 'N', name = 'N'})
-    self.area:addGameObject('Score', 400+90,  80, {scoreCount = 'G', name = 'G'})
-    
-    self.area:addGameObject('Rectangle', 0,   0,   {width = 800, height = 20,  name = 'topBoarder'   })
-    self.area:addGameObject('Rectangle', 0,   580, {width = 800, height = 20,  name = 'bottomBoarder'})
-    self.area:addGameObject('Rectangle', 0,   0,   {width = 20,  height = 600, name = 'leftBoarder'  })
-    self.area:addGameObject('Rectangle', 780, 0,   {width = 20,  height = 600, name = 'rightBoarder' })
-    
-    self.area:addGameObject('Text', 400, 240, {words = 'Single Player', center = true, name = 'SinglePlayer'})
-    self.area:addGameObject('Text', 400, 300, {words = 'Multiplayer',   center = true, name = 'Multiplayer' })
-    self.area:addGameObject('Text', 400, 360, {words = 'Settings',      center = true, name = 'Settings'    })
-
-    self.area:addGameObject('MenuScroll', 0, 0, {locations = {'SinglePlayer','Multiplayer','Settings'}, width = 150, height = 2, center = true, name = 'MenuScroll'})
+    self.area:addGameObject('Score',      400-150, 80,  {name = 'P',             scoreCount = 'P' })
+    self.area:addGameObject('Score',      400-70,  80,  {name = 'O'                               })
+    self.area:addGameObject('Score',      400+10,  80,  {name = 'N',             scoreCount = 'N' })
+    self.area:addGameObject('Score',      400+90,  80,  {name = 'G',             scoreCount = 'G' })
+    self.area:addGameObject('Rectangle',  0,       0,   {name = 'topBoarder',    width = 800, height = 20  })
+    self.area:addGameObject('Rectangle',  0,       580, {name = 'bottomBoarder', width = 800, height = 20  })
+    self.area:addGameObject('Rectangle',  0,       0,   {name = 'leftBoarder',   width = 20,  height = 600 })
+    self.area:addGameObject('Rectangle',  780,     0,   {name = 'rightBoarder',  width = 20,  height = 600 })
+    self.area:addGameObject('Text',       400,     240, {name = 'SinglePlayer',  words = 'Single Player', center = true })
+    self.area:addGameObject('Text',       400,     300, {name = 'Multiplayer',   words = 'Multiplayer',   center = true })
+    self.area:addGameObject('Text',       400,     360, {name = 'Settings',      words = 'Settings',      center = true })
+    self.area:addGameObject('MenuScroll', 0,       0,   {name = 'MenuScroll',    locations = {'SinglePlayer','Multiplayer','Settings'}})
 end
 function Menu:update(dt)
     self.area:update(dt)
