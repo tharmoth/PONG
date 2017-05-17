8d2c71c2eff9a20d684238b9f6c0b12c56e64c59
tharmoth <tharmoth@gmail.com>
2017-05-17 14:09:41 -0500

    Update area.lua

--- rooms/area.lua
+++ rooms/area.lua
@@ -4,6 +4,7 @@ function Area:new(room)
     self.room = room
     self.game_objects = {}
 end
+--! Remove from collision box if dead
 function Area:update(dt)
     for i = #self.game_objects, 1, -1 do
         local game_object = self.game_objects[i]
@@ -14,6 +15,7 @@ end
 function Area:draw()
     for _, game_object in ipairs(self.game_objects) do game_object:draw() end
 end
+--! Add to collision box here
 function Area:addGameObject(game_object_type, x, y, opts)
     local opts = opts or {}
     local game_object = _G[game_object_type](self, x or 0, y or 0, opts)
