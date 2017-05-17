81c4eae7a0bd9b5b7ea8ecdd8ddc431e4cac12c4
tharmoth <tharmoth@gmail.com>
2017-05-17 08:42:58 -0500

    Update player.lua

--- objects/player.lua
+++ objects/player.lua
@@ -1,8 +1,7 @@
 Player = Rectangle:extend()
 function Player:new(area, x, y, opts)
     Player.super.new(self, area, x, y, opts)
-    self.speedy = 0
-    self.score = 0
+    self.score, self.speedy = 0, 0
     input:bind(self.upKey, self.upKey)
     input:bind(self.downKey, self.downKey)
 end
@@ -13,18 +12,10 @@ function Player:update(dt)
     self:stopCollision("topBoarder",bottomBounce)
 end
 function Player:playerInput()
-    if input:pressed(self.upKey) then 
-      self.speedy = self.speedy - 500 
-    end
-    if input:released(self.upKey) then 
-      self.speedy = self.speedy + 500 
-    end
-    if input:pressed(self.downKey) then 
-      self.speedy = self.speedy + 500 
-    end
-    if input:released(self.downKey) then 
-      self.speedy = self.speedy - 500 
-    end
+    if input:pressed(self.upKey) then self.speedy = self.speedy - 500 end
+    if input:released(self.upKey) then self.speedy = self.speedy + 500 end
+    if input:pressed(self.downKey) then self.speedy = self.speedy + 500 end
+    if input:released(self.downKey) then self.speedy = self.speedy - 500 end
 end
 --! Checks if playyer intersects passed object if so does passed function
 function Player:stopCollision(object,funct)
@@ -34,10 +25,10 @@ function Player:stopCollision(object,funct)
     end
 end
 --! Following bounce functions are passed to detectCollision to perfrom effects on the player when it bounces
-function topBounce(self,collisionObject)
+local function topBounce(self,collisionObject)
   self.y = self.area.game_objects[collisionObject].y-self.height
 end
-function bottomBounce(self,collisionObject)
+local function bottomBounce(self,collisionObject)
   self.y = self.area.game_objects[collisionObject].y+self.area.game_objects[collisionObject].height
 end
 function Player:draw()
